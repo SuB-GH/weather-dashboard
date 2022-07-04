eb6b91170766325a5f6c7d7448ad30b44dc45c2e
Susan Brown <brown684408@gmail.com>
2022-07-03 19:19:08 -0500

    add functionality to form button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,21 @@
     // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=54848&appid=3e89c17611e41ba25f1b674bd5f9012d";
     // fetch("https://api.openweathermap.org/data/2.5/weather?zip=54848&appid=3e89c17611e41ba25f1b674bd5f9012d")
 
-     var inputForm = document.querySelector("#user-form");
+    var inputForm = document.querySelector("#user-form");
     var dailyForecast = document.querySelector('ul'); // sample from activity 6
     var fetchButton = document.getElementById('fetch-button');
+    var inputContainerEl = document.querySelector("#input-container");
+    var cityInputEl = document.querySelector("#city");
+    var username = cityInputEl.value.trim();
 
-    var formSubmitHandler = function(event) {
-        // prevent page from refreshing
-        event.preventDefault();
-    }
-    
+   
     inputForm.addEventListener("submit", formSubmitHandler);
 
     var formSubmitHandler = function(event) {
         // prevent page from refreshing
-        event.preventDefault(); 
-      
+        event.preventDefault();       
     }      
+
     console.log(formSubmitHandler);
     
  //var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=54848&appid=3e89c17611e41ba25f1b674bd5f9012d'; //this url works
@@ -43,16 +42,20 @@
         });
     }
     getApi();
-
-    
   
-    
+  
     console.log(getApi);
 
 
      var getWeather = function (cityID) {
       var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=3e89c17611e41ba25f1b674bd5f9012d" + cityID;
       
+      var displayRepos = function (repos, searchTerm) {
+        // check if api returned any repos
+    if (repos.length === 0) {
+      inputContainerEl.textContent = "No repositories found.";
+      return;
+    }
       fetch(apiUrl).then(function (response) {
           // request was successful
           if (response.ok) {
@@ -62,8 +65,11 @@
               });
           } else {
               alert("There was a problem with your request!");
-          }
-  
+          }}
+          .catch(function(error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
+          }));
           if (response.ok) {
               response.json().then(function (data) {
                   displayIssues(data);
@@ -73,13 +79,53 @@
                       console.log("displayWarning(cityID)");
                   }
               });
-          }
+          }}}
       
       console.log(getWeather);
-      })
-  };
+      
+  
   getWeather();
 
+//test out lines 103-...
+
+for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+  
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+  
+        // append to container
+        repoEl.appendChild(titleEl);
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // clear old content
+      repoContainerEl.textContent = "";
+      repoSearchTerm.textContent = searchTerm;
+
+      // check if current repo has issues or not
+      if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+      } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
+
+      // append to container
+      repoEl.appendChild(statusEl);
+      // append container to the dom
+      repoContainerEl.appendChild(repoEl);
+    }
+    console.log(repos);
+    console.log(searchTerm);
+  
 
 //        //fetch("https://api.openweathermap.org/data/2.5/weather?zip=54848&appid=3e89c17611e41ba25f1b674bd5f9012d");
 //         //make a request to the url
